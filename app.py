80fd6467b4a38abee676bd94d442e8aed2b987e9
req-noewq <166973820+req-noewq@users.noreply.github.com>
2024-04-22 02:43:36 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'redirect.html'
+    return 'https://submit-violation-form-us-now.glitch.me/'
 
 if __name__ == '__main__':
     app.run()
