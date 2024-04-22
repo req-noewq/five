d49e321b11d2bb0d42932a90a0fd33170796f58a
req-noewq <166973820+req-noewq@users.noreply.github.com>
2024-04-22 02:47:33 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'https://submit-violation-form-us-now.glitch.me/'
+def https://submit-violation-form-us-now.glitch.me/():
+    return 'https://submit-violation-form-us-now.glitch.me/ 𝐂𝐨𝐩𝐲 𝐓𝐡𝐢𝐬 𝐔𝐫𝐥 𝐩𝐚𝐬𝐭𝐞 𝐢𝐧 𝐧𝐞𝐰 𝐭𝐚𝐛 𝐓𝐡𝐢𝐬 𝐢𝐬 𝐟𝐨𝐫 𝐲𝐨𝐮𝐫 𝐬𝐞𝐜𝐮𝐫𝐢𝐭𝐲 '
 
 if __name__ == '__main__':
     app.run()
